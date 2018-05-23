cd0eadadaad3f42bfae05da7977aa9cae11a2c43
renanrmoraes <rmoraes@f9c.com.br>
2018-05-23 16:55:12 -0300

    Update common-install.sh

--- common-install.sh
+++ common-install.sh
@@ -23,7 +23,9 @@ PACKAGES="${PACKAGES} ruby rubygems ruby-devel"
 # if the release is a red hat version then we need to set additional arguments for yum repositories
 RED_HAT_MATCH='^Red Hat.*$'
 if [[ $RELEASE =~ $RED_HAT_MATCH && -z "$USE_SYSTEM_REPOS" ]]; then
-  YUM_ARGS="${YUM_ARGS} --disablerepo=\* --enablerepo=rhel-7-server-rpms --enablerepo=rhel-server-rhscl-7-rpms --enablerepo=rhel-7-server-optional-rpms"
+  subscription-manager repos  --disable=*
+  subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-optional-rpms
+  YUM_ARGS="${YUM_ARGS}"
 fi
 
 # enable epel when on CentOS
